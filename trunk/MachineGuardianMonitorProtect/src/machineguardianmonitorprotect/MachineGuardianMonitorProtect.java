fa859b6a139bccbef340d27ffe42d4fb473ce6b9
mrezzosoftware@gmail.com <mrezzosoftware@gmail.com@f35d19ec-7eba-7c96-d6e6-c38653d08a76>
2012-05-08 02:28:29 +0000

    Testes - Funções de leitura de teclas.

--- trunk/MachineGuardianMonitorProtect/src/machineguardianmonitorprotect/MachineGuardianMonitorProtect.java
+++ trunk/MachineGuardianMonitorProtect/src/machineguardianmonitorprotect/MachineGuardianMonitorProtect.java
@@ -1,6 +1,7 @@
 package machineguardianmonitorprotect;
 
 import machineguardianmonitorprotect.core.ActiveWindowInfo;
+import machineguardianmonitorprotect.core.KeyboardHook;
 
 public class MachineGuardianMonitorProtect {
 
@@ -9,6 +10,8 @@ public class MachineGuardianMonitorProtect {
      */
     public static void main(String[] args) {
         ActiveWindowInfo janelaAtiva = new ActiveWindowInfo();
+        KeyboardHook kbh = new KeyboardHook();
+        kbh.capturarTeclasDigitadas();
         System.out.println("Título janela ativa: " + janelaAtiva.getTituloJanelaAtiva());
         System.out.println("Nome executável da janela: " + janelaAtiva.getNomeExecutavelJanelaAtiva());
     }
