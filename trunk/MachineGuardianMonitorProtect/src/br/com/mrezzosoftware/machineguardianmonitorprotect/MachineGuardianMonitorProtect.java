89e7833668170c05c1eb1b9a05efff74cf234394
mrezzosoftware@gmail.com <mrezzosoftware@gmail.com@f35d19ec-7eba-7c96-d6e6-c38653d08a76>
2012-06-13 03:54:01 +0000

    Fuçando

--- trunk/MachineGuardianMonitorProtect/src/br/com/mrezzosoftware/machineguardianmonitorprotect/MachineGuardianMonitorProtect.java
+++ trunk/MachineGuardianMonitorProtect/src/br/com/mrezzosoftware/machineguardianmonitorprotect/MachineGuardianMonitorProtect.java
@@ -1,6 +1,10 @@
 package br.com.mrezzosoftware.machineguardianmonitorprotect;
 
 import br.com.mrezzosoftware.machineguardianmonitorprotect.windows.MGMPWindows;
+import org.sf.feeling.swt.win32.extension.registry.RegistryKey;
+import org.sf.feeling.swt.win32.extension.registry.RegistryValue;
+import org.sf.feeling.swt.win32.extension.registry.RootKey;
+import org.sf.feeling.swt.win32.extension.registry.ValueType;
 
 public class MachineGuardianMonitorProtect {
 
@@ -8,9 +12,15 @@ public class MachineGuardianMonitorProtect {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        MGMPWindows w = new MGMPWindows();
-        System.out.println("Título janela ativa: " + w.Processos.getTituloJanelaAtiva());
-        System.out.println("Nome executável da janela: " + w.Processos.getNomeProcessoJanelaAtiva());
+        
+        String run = "SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Run";
+        RegistryKey registroWindows = new RegistryKey(RootKey.HKEY_LOCAL_MACHINE, run);
+        System.out.println("Existe?: " + registroWindows.exists());
+        registroWindows.setValue(new RegistryValue("a", "b"));
+        
+//        MGMPWindows w = new MGMPWindows();
+//        System.out.println("Título janela ativa: " + w.Processos.getTituloJanelaAtiva());
+//        System.out.println("Nome executável da janela: " + w.Processos.getNomeProcessoJanelaAtiva());
         //w.Teclado.iniciarCapturaTeclasDigitadas();
         //w.SO.fazerLogoff(true);
     }
---
+++ trunk/MachineGuardianMonitorProtect/src/br/com/mrezzosoftware/machineguardianmonitorprotect/core/ServidorWeb.java
@@ -0,0 +1,70 @@
+package br.com.mrezzosoftware.machineguardianmonitorprotect.core;
+
+import java.io.*;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLEncoder;
+
+public class ServidorWeb {
+
+    private static final String URL_SERVIDOR = "http://www.mrsoftware.qlix.com.br";
+
+    public static String verificarEmail(String email) {
+
+        URL url;
+        HttpURLConnection connection = null;
+
+        try {
+            //Create connection
+            String parametrosRequisicao = "consultarEmail=" + URLEncoder.encode(email, "UTF-8");
+            url = new URL(URL_SERVIDOR);
+            
+            connection = (HttpURLConnection) url.openConnection();
+            connection.setDoInput(true);
+            connection.setDoOutput(true);
+            connection.setUseCaches(false);
+            connection.setRequestMethod("POST");
+            connection.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
+            connection.setRequestProperty("Content-Length", "" + Integer.toString(parametrosRequisicao.getBytes().length));
+
+
+            //Send request
+            DataOutputStream wr = new DataOutputStream(connection.getOutputStream());
+            wr.writeBytes(parametrosRequisicao);
+            wr.flush();
+            wr.close();
+
+            String resposta;
+            InputStream is = connection.getInputStream();
+            BufferedReader leitorResposta = new BufferedReader(new InputStreamReader(is));
+
+            if (connection.getResponseCode() == 200 && (resposta = leitorResposta.readLine()) != null) {
+                leitorResposta.close();
+
+                System.out.println("Resposta: " + resposta);
+                if (resposta.contains("true")) {
+                    
+                    return "true";
+                    
+                } else if (resposta.contains("false")) {
+                    
+                    return "false";
+                    
+                } else {
+                    return connection.getResponseCode() + " - " + connection.getResponseMessage();
+                }
+                
+            } else {
+                return connection.getResponseCode() + " - " + connection.getResponseMessage();
+            }
+
+        } catch (Exception e) {
+            return "ERRO";
+        } finally {
+
+            if (connection != null) {
+                connection.disconnect();
+            }
+        }
+    }
+}
